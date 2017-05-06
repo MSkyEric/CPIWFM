996d686d849376eaeee3fa40c2734de819896abe
Eric Deng <graydun@hotmail.com>
2017-05-06 18:30:06 +0800

    添加项目文件。

---
+++ CPIWFM/Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPIWFM
+{
+    class Context
+    {
+        Strategy oStrategy;
+        public Context(Strategy oStrategy)
+        {
+            this.oStrategy = oStrategy;
+        }
+        public void ContextInterface()
+        {
+            oStrategy.AlgorithmForTheStrategy();
+        }
+    }
+}
